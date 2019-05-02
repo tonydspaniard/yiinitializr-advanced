dca2e950e13290dbcf84e69a0b8e7f244d534019
Antonio Ramirez <antonio@ramirezcobos.com>
2019-05-02 19:16:21 +0200

    Update EApiActiveRecord.php

--- api/extensions/components/EApiActiveRecord.php
+++ api/extensions/components/EApiActiveRecord.php
@@ -4,8 +4,7 @@
  *
  * Add here all API helper functions to be shared among your API models
  *
- * @author Antonio Ramirez <amigo.cobos@gmail.com>
- * @link http://www.ramirezcobos.com/
+ * @author Antonio Ramirez <hola@2amigos.us>
  * @link http://www.2amigos.us/
  * @copyright 2013 2amigOS! Consultation Group LLC
  * @license http://www.opensource.org/licenses/bsd-license.php New BSD License
